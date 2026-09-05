d1f5179ab667611c9226b0cf6344e1ab0f2d9e36
Jyotishna03 <jyotishnasoni0817@gmail.com>
2026-09-06 00:53:30 +0530

    Time: 0 ms (100%), Space: 28.5 MB (39.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,28 +5,26 @@ public:
         stack<int> st;
         vector<int> res(n, -1);
 
-        // First loop: put elements from right to left
-        for(int i = n - 2; i >= 0; i--) {
-            while(!st.empty() && st.top() <= nums[i]) {
+        for(int i = n - 2; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i]) 
+            {
                 st.pop();
+            }    
+            st.push(nums[i]);
+        }
+        for(int i = n - 1; i >= 0; i--){
+
+            while(!st.empty() && st.top() <= nums[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+
+                res[i] = st.top();   
+
             }
             st.push(nums[i]);
         }
-
-        // Second loop: find next greater
-        for(int i = n - 1; i >= 0; i--) {
-
-            while(!st.empty() && st.top() <= nums[i]) {
-                st.pop();
-            }
-
-            if(!st.empty()) {
-                res[i] = st.top();
-            }
-
-            st.push(nums[i]);
-        }
-
         return res;
     }
+   
 };
